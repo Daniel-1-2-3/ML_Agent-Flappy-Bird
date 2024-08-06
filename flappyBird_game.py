d422cb9d55509a56621d2703dcdd6a2e656a47b8
Daniel-1-2-3 <danielzhu501@gmail.com>
2024-08-06 19:46:14 -0400

    create bird

--- flappyBird_game.py
+++ flappyBird_game.py
@@ -15,3 +15,9 @@ PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25 #rotation used later for tilting the bird, during up/down movement 
+    ROT_VEL = 20 #rotation velocity, speed at which the bird rotates (number of degrees rotated per frame)
+    ANIMATION_TIME = 5
+
